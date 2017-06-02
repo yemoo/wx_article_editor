b96b84fa97f2f9121eca76fecf511819de1d2af6
wei.yan <wei.yan@ifchange.com>
2017-06-02 16:09:59 +0800

    node入口打包中文不转义

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,25 @@ const packer = require('packer');
 const gulpIf = require('gulp-if');
 const babel = require('gulp-babel');
 
-const jsMinify = uglify({
-    output: {
-        // 避免对'\uxxxx'的反转，强制将所有unicode转为ascii
-        ascii_only: true
-    },
-    compress: {
-        // unsafe: true
-    }
-});
-
 gulp.task('minifyNode', function () {
     return gulp.src('index.jsx')
         .pipe(babel({
-            "minified": true,
+            // "minified": true,
             "comments": false,
             "presets": [["es2015", {loose: true}]]
         }))
+        .pipe(uglify())
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('minifyAssets', function () {
     return gulp.src(['public/editor.js', 'public/editor.css'])
-        .pipe(gulpIf(/\.js$/, jsMinify))
+        .pipe(gulpIf(/\.js$/, uglify({
+            output: {
+                // 避免对'\uxxxx'的反转，强制将所有unicode转为ascii
+                ascii_only: true
+            }
+        })))
         .pipe(gulpIf(/\.js$/, through.obj(function (file, enc, cb) {
             var contents = file.contents.toString();
             file.contents = new Buffer(packer.pack(contents, true, true));
